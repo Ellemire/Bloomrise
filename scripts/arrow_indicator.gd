bf2433dac28a23addb6ed1966870f50b4d12b257
Ellemire <julia.ellemire@gmail.com>
2025-04-12 21:41:20 +0200

    Added day night indicator arrows

---
+++ scripts/arrow_indicator.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _ready():
+	DayAndNightCycleManager.time_tick.connect(update_arrow)
+
+func update_arrow(day: int, hour: int, minute: int) -> void:
+	# Ukryj wszystkie
+	$ArrowTransition.visible = false
+	$ArrowDay.visible = false
+	$ArrowNight.visible = false
+
+	# Aktywuj odpowiednią strzałkę
+	if hour >= 7 and hour < 18:
+		$ArrowDay.visible = true
+	elif hour >= 20 or hour < 5:
+		$ArrowNight.visible = true
+	else:
+		$ArrowTransition.visible = true
